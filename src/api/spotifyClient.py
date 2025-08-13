451c387c09bd78bcb99782ba5521e0d5c8dd721d
Ansh Saini <anshbsaini84@gmail.com>
2025-08-13 16:16:41 +0530

    Revert "API Client folder Structure"
    This reverts commit ced6e13cb39b2b2dbaccca0c8f897dcf1b5ea35c.

--- src/api/spotifyClient.py
+++ src/api/spotifyClient.py
@@ -1,13 +1,10 @@
 import os
-from telnetlib import XAUTH
-
 import requests
 from dotenv import load_dotenv
 
 class SpotifyClient:
     def __init__(self):
-        load_dotenv(dotenv_path="config/.env")
-
+        load_dotenv()
         self.clientId = os.getenv("SPOTIFY_CLIENT_ID")
         self.clientSecret = os.getenv("SPOTIFY_CLIENT_SECRET")
         self.tokenUrl = "https://accounts.spotify.com/api/token"
@@ -19,15 +16,7 @@ class SpotifyClient:
         1. Sends clientId and clientSecret to Spotify
         2. Get the access token from Spotify and stores it
         """
-        authResponse = requests.post(self.tokenUrl,
-                                     data = {"grant_type": "client_credentials"},
-                                     auth = (self.clientId, self.clientSecret))
-        if authResponse.status_code != 200:
-            raise Exception(f"Failed to authenticate with Spotify: {authResponse.status_code}")
-
-        tokenData = authResponse.json()
-        self.accessToken = tokenData["access_token"]
-        return self.accessToken
+        pass
 
     def searchTrack(self, query, limit=10):
         """
--- src/main.py
+++
@@ -1,6 +0,0 @@
-from api.spotifyClient import SpotifyClient
-
-if __name__ == '__main__':
-    client = SpotifyClient()
-    token = client.authenticate()
-    print(f"Access Token: {token}")
