a276257039e93d41e00f5fe4ba108d8be6ae4b64
Ansh Saini <anshbsaini84@gmail.com>
2025-08-18 21:11:55 +0530

    Song and Artist Details

--- src/main.py
+++ src/main.py
@@ -1,10 +1,30 @@
-## Testing the API to see if spotify is replying to my request
+## Testing the API to see if Spotify is replying to my request
 
 from api.spotifyClient import SpotifyClient
 
 if __name__ == '__main__':
     client = SpotifyClient()
-    print(client.clientId)
-    print(client.clientSecret)
     token = client.authenticate()
-    print(f"The access token is: {token}")
+
+##Searching for the tracks
+
+    tracks = client.searchTrack("Blinding Lights", limit=3)
+    print("Search results: ")
+    for t in tracks:
+        print(f"{t['trackName']} by {t['artistName']} "
+              f"(Album: {t['albumName']}, Released: {t['releaseDate']})")
+
+##Getting the details for the track (metadata of first track only)
+
+    if tracks:
+        trackID = tracks[0]['trackID']
+        print(f"\nFetching details for track: {tracks[0]['trackName']}")
+        songDetails = client.getSongDetails(trackID)
+        print("Song Details:", songDetails)
+
+##Gets the details of the first artist of that track
+
+        artistID = tracks[0]['artistID']
+        print(f"\nFetching details for artist: {tracks[0]['artistName']}")
+        artistDetails = client.getArtistDetails(artistID)
+        print("Artist Details:", artistDetails)
