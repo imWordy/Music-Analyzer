495a9b6fbd6ee96a54385fbbf928552c53bf8dc1
Anshumaan Soni <102132714+TETRAWasTaken@users.noreply.github.com>
2025-10-14 04:55:59 +0000

    GUI-3

---
+++ src/api/reccobeatsApi.py
@@ -0,0 +1,49 @@
+import http.client
+from typing import List, Dict
+
+"""
+An API adapter to retrieve data from reccobeats
+"""
+
+class reccobeats:
+    def __init__(self):
+        self.BaseURL = "https://api.reccobeats.com"
+    
+    def request(method: str, payload: str) -> Dict:
+        """
+        Adapter to make requests to the API
+        
+        Args:
+            method: The type of API call
+            payload: The input parameters of the API call
+        
+        Returns:
+            Dictionary for the Returned JSON
+        """
+        conn = http.client.HTTPSConnection("api.reccobeats.com")
+        header = {
+            'Accept': 'application/json'
+        }
+        conn.request("GET", f"/v1/{method}", payload, header)
+        res = conn.getresponse()
+        data=res.read()
+        return data.decode("utf-8")
+
+    def getmany_Audio_Features(self, tracks: List) -> Dict:
+        """
+        Wrapper to perform correct API call for audio features
+        
+        Args:
+            tracks: A list of all the tracks to get Audio features for
+
+        Returns:
+            Dictionary of Returned JSON
+        """
+        payload = ""
+        for i in tracks:
+            payload = payload + i + ','
+        
+        return self.request("audio-features", payload)
+    
+
+
